38b9e062011d3b12d320750ca1d47eae06e07e3f
akshay ashok <akshayashokan1054@gmail.com>
2025-05-11 20:47:37 +0530

    optimized imports

--- composeApp/src/commonMain/kotlin/com/akshayashokcode/bird_app_compose_kmm/App.kt
+++ composeApp/src/commonMain/kotlin/com/akshayashokcode/bird_app_compose_kmm/App.kt
@@ -1,7 +1,6 @@
 package com.akshayashokcode.bird_app_compose_kmm
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,22 +16,20 @@ import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import bird_app_compose_kmm.composeapp.generated.resources.Res
-import bird_app_compose_kmm.composeapp.generated.resources.compose_multiplatform
 import com.akshayashokcode.bird_app_compose_kmm.model.BirdImage
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun BirdAppTheme(
